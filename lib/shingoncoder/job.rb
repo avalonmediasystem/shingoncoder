3b8d35938a76ddc90692bdda25e5936748a70375
Justin Coyne <jcoyne@justincoyne.com>
2015-09-28 09:12:46 -0500

    Add API documentation

--- lib/shingoncoder/job.rb
+++ lib/shingoncoder/job.rb
@@ -3,6 +3,7 @@ module Shingoncoder
     class << self
       # @param [Hash] options
       # @option [String] :input the url to the file
+      # @return [Response]
       def create(options = {})
         id = Backend::Job.create(options)
         # TODO in the future the backend could be via a HTTP API
@@ -15,6 +16,7 @@ module Shingoncoder
 
       # Each output should have current_event
       # job itself should have 'progress'
+      # @return [Response]
       def progress(id)
         job = Shingoncoder::Backend::JobRegistry::Job.find(id)
         Response.new('body' => id, 'code' => 200)
@@ -23,6 +25,7 @@ module Shingoncoder
       # should have ["job"]["input_media_file"]["url"]
       # TODO should have ["job"]["state"] which is one of:
       #   "pending", "waiting", "processing", "cancelled", "failed", "finished"
+      # @return [Response]
       def details
         job = Shingoncoder::Backend::JobRegistry::Job.find(id)
         Response.new('body' => { 'job' => { 'input_media_file' => { 'url' => job.input.fetch('input') } } }, 'code' => 200)
